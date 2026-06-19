f8173d863b9e4fc966c113d7c3b9db2ebf46ce04
Adham Hamada <adhamhamada.112233@gmail.com>
2026-06-19 22:56:59 +0300

    update site

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,13 +1,44 @@
+import fs from "fs";
+import path from "path";
+
 export async function POST(req: Request) {
 
 const body = await req.json();
 
 const email = body.email;
 
-console.log("NEW EMAIL:", email);
+const file =
+path.join(
+process.cwd(),
+"data",
+"emails.json"
+);
+
+const current =
+JSON.parse(
+fs.readFileSync(
+file,
+"utf8"
+)
+);
+
+current.push({
+email,
+date:
+new Date(),
+});
+
+fs.writeFileSync(
+file,
+JSON.stringify(
+current,
+null,
+2
+)
+);
 
 return Response.json({
-success: true,
+success:true,
 });
 
 }
