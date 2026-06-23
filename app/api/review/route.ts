d6da7a165c2871e076c8c1e3d206ccc3f59d8ac2
Adham Hamada <adhamhamada.112233@gmail.com>
2026-06-23 23:47:07 +0300

    review

---
+++ app/api/review/route.ts
@@ -0,0 +1,49 @@
+import { supabase } from "@/lib/supabase";
+
+export async function POST(
+req: Request
+) {
+
+const body =
+await req.json();
+
+const {
+error
+} =
+await supabase
+.from("reviews")
+.insert([
+{
+name:
+body.name,
+
+review:
+body.review,
+},
+]);
+
+if (
+error
+) {
+
+console.log(
+error
+);
+
+return Response.json(
+{
+success:false
+},
+{
+status:500
+}
+);
+
+}
+
+return Response.json(
+{
+success:true
+});
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import Reviews
+from "@/components/Reviews";
 
 
 export default function Home() {
