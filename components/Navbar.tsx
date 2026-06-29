425062f94e657fabdda7344fd33b854db67f1d87
Adham Hamada <adhamhamada.112233@gmail.com>
2026-06-29 04:48:43 +0300

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -161,12 +161,11 @@ className="hover:opacity-60 duration-300"
 >
 Contact
 </a>
-
+{false && (
 <Link
 href="/cart"
 className="relative hover:opacity-60 duration-300"
 >
-
 <svg
 width="22"
 height="22"
@@ -175,51 +174,35 @@ fill="none"
 stroke="currentColor"
 strokeWidth="1.6"
 >
-
 <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/>
-
 <line x1="3" y1="6" x2="21" y2="6"/>
-
 <path d="M16 10a4 4 0 0 1-8 0"/>
-
 </svg>
-
 {
-
 cartCount>0 && (
-
 <span
 className="
 absolute
 -top-2
 -right-2
-
 w-4
 h-4
-
 rounded-full
-
 bg-[#55614A]
-
 text-white
-
 text-[10px]
-
 flex
 items-center
 justify-center
 "
 >
-
 {cartCount}
-
 </span>
-
 )
-
 }
 
 </Link>
+)}
 
 </div>
 
